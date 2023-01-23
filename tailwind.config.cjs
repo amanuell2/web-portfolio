4fef54d628281321e2295fcb757723fd1f8b1e1d
amanuel <amanuel0000@gmail.com>
2023-01-23 17:34:29 +0300

    feat: add tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      "black": '#000000',
+      'white': '#ffffff',
+      "slate-dark-gray": "#282C33",
+      "pastel-red": "#E06B74",
+      "fern-green": "#98C379",
+      "desert-sand": "#E5C07A",
+      "sky-blue": "#62AEEF",
+      "lilac": "#C778DD",
+      "turquoise-blue": "#55B6C2",
+      "slate-light-gray": "#ABB2BF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
