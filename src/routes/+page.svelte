43889a1390f029ffba5fe4c55f3edf5547f1ccba
amanuel <amanuel0000@gmail.com>
2023-01-23 17:35:15 +0300

    feat: add page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Icon from 'svelte-awesome';
+  import { github, linkedin, twitter,} from 'svelte-awesome/icons';
+</script>
+
+<div class=" container mx-auto">
+  <div class="bg-slate-dark-gray mx-16 h-screen flex flex-row justify-between">
+    <div class="fixed top-0 z-10 mx-8">
+      <div class="flex flex-col justify-center items-center text-center text-slate-light-gray">
+        <div class="w-px h-44 bg-slate-light-gray"></div>
+        <a href="https://linkedin.com" class="my-1 text-2xl  hover:text-white">
+          <Icon data={github} scale={1.5}/>
+        </a>
+        <a href="https://github.com" class="my-1 text-2xl  hover:text-white">
+          <Icon data={linkedin} scale={1.5}/>
+        </a>
+        <a href="https://twitter.com" class="my-1 text-2xl  hover:text-white">
+          <Icon data={twitter} scale={1.5}/>
+        </a>
+      </div>
+    </div>
+    
+   <div class="flex-1 ml-16">
+    <p class="text-white text-2xl"> this is a starting point </p>
+   </div>
+  </div>
+</div>
+
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.black);
+    }
+  </style>
